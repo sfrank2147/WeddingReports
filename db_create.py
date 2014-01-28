d1062576cb81b2fc1fee4b1bf74cf761051a9e16
Sam Frank <sfrank2147@gmail.com>
2014-01-28 09:13:17 -0500

    changing virtualenv

--- db_create.py
+++ db_create.py
@@ -3,9 +3,6 @@
 #script to create a database using SQLAlchemy
 from config import SQLALCHEMY_DATABASE_URI
 from wedding_app import db
-import os.path
 
 #create all the tables we've defined
 db.create_all()
-
-
