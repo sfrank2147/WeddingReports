c0064a6d79a085dc4f0e4c5e4afa8dad1beffb94
Sam Frank <sfrank2147@gmail.com>
2014-02-11 17:18:29 -0500

    Drop-down menu for rating venues

--- wedding_app/forms.py
+++ wedding_app/forms.py
@@ -1,11 +1,11 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, PasswordField, TextAreaField, IntegerField
+from wtforms import TextField, PasswordField, TextAreaField, SelectField
 from wtforms.validators import Required
 
 class AddReportForm(Form):
     venue_name = TextField('venue_name')
     content = TextAreaField('content')
-    rating = IntegerField('rating')
+    rating = SelectField(u'Rating', choices=[(str(x),str(x)) for x in range(1,11)])
 
 class SearchForm(Form):
     venue_name = TextField('venue_name')
